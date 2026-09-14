8968ec1c43e057371f0c7055ba94b64fabb8cc77
liban1ali43 <libanali160@gmail.com>
2026-09-14 16:30:10 +0100

    Add Flask app with MySQL connection

---
+++ flask_mysql_connect.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import MySQLdb
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    db = MySQLdb.connect(
+        host="mydb",
+        user="root",
+        passwd="my-secret-pw",
+        db="mysql"
+    )
+    cur = db.cursor()
+    cur.execute("SELECT VERSION()")
+    version = cur.fetchone()
+    return f'Hello, world! MySQL version: {version[0]}'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5003)
